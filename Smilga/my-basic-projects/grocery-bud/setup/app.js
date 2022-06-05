3a95ac810627527d90fa314f90f5540f3c7ea8e0
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-06-05 15:09:01 +0100

    editItem()

--- Smilga/my-basic-projects/grocery-bud/setup/app.js
+++ Smilga/my-basic-projects/grocery-bud/setup/app.js
@@ -16,7 +16,7 @@ let editID = "";
 // submit form
 form.addEventListener("submit", addItem);
 // clear items
-clearBtn.addEventListener('click', clearItems)
+clearBtn.addEventListener("click", clearItems);
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault();
@@ -40,10 +40,10 @@ function addItem(e) {
         <i class="fas fa-trash"></i>
       </button>
     </div>`;
-    const deleteBtn = element.querySelector('.delete-btn')
-    const editBtn = element.querySelector('.edit-btn')
-    deleteBtn.addEventListener('click',deleteItem)
-    editBtn.addEventListener('click',editItem)
+    const deleteBtn = element.querySelector(".delete-btn");
+    const editBtn = element.querySelector(".edit-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
     // append child
     list.appendChild(element);
     // display alert
@@ -51,11 +51,13 @@ function addItem(e) {
     // show container
     container.classList.add("show-container");
     // add to local storage
-    addToLocalStorage(id, value)
+    addToLocalStorage(id, value);
     // set back to default
-    setBackToDefault() 
+    setBackToDefault();
   } else if (value && editFlag) {
-    console.log("editing");
+      editElement.innerHTML = value
+      displayAlert('value changed', 'success')
+      setBackToDefault()
   } else {
     displayAlert("please enter value", "danger");
   }
@@ -74,46 +76,56 @@ function displayAlert(text, action) {
 }
 // set back to default
 function setBackToDefault() {
-    grocery.value = ""
-    editFlag = false
-    editID = ""
-    submitBtn.textContent = "submit"
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "submit";
 }
 // clear items
 function clearItems() {
-    const items = document.querySelectorAll(".grocery-item")
+  const items = document.querySelectorAll(".grocery-item");
 
-    if (items.length > 0) {
-        items.forEach(function (item) {
-            list.removeChild(item)
-        })
-    }
-    container.classList.remove("show-container")
-    displayAlert("empty list", "danger")
-    setBackToDefault() // useful when editing
-    // localStorage.removeItem('list')
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger");
+  setBackToDefault(); // useful when editing
+  // localStorage.removeItem('list')
 }
 // delete function
 function deleteItem(e) {
-    const element = e.currentTarget.parentElement.parentElement
-    const id = element.dataset.id;
-    list.removeChild(element)
-    if(list.children.length === 0) {
-        container.classList.remove("show-container")
-    }
-    displayAlert('item removed', 'danger')
-    setBackToDefault()
-    // remove from local storage
-    // removeFromLocalStorage(id)
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove("show-container");
+  }
+  displayAlert("item removed", "danger");
+  setBackToDefault();
+  // remove from local storage
+  // removeFromLocalStorage(id)
 }
 
 // edit function
-function editItem() {
-    console.log("edit item");
+function editItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    // set edit item
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    // set form value
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    //
+    submitBtn.textContent = "edit";
 }
 
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
-    console.log("added to local storage");
+  console.log("added to local storage");
 }
+function removeFromLocalStorage(id) {}
+function editLocalStorage(id, value) {}
 // ****** SETUP ITEMS **********
