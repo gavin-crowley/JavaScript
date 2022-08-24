fe2d9095bc16398aebb5ec4c1726f97ce1d649ea
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-08-24 10:19:17 +0100

    aug 24

--- JSB2P/2-my-sols/ch5.js
+++ JSB2P/2-my-sols/ch5.js
@@ -1,7 +1,7 @@
 // 5.1
 const MAX_VALUE = 5;
 const randNum = Math.floor(Math.random() * MAX_VALUE) + 1;
-console.log(randNum);
+// console.log(randNum);
 let correct = false;
 
 // while (!correct) {
@@ -30,31 +30,44 @@ for (let i = 1; i <= 10; i++) {
   myWork.push({ name: `Lesson ${i}`, status: i % 2 !== 0 });
 }
 
-console.log(myWork);
+// console.log(myWork);
 
 // 5.4
 
+// const myTable = [];
+// const rows = 4;
+// const cols = 7;
+// let counter = 0;
+// for (let y = 0; y < rows; y++) {
+//   let tempTable = [];
+//   for (let x = 0; x < cols; x++) {
+//     counter++;
+//     tempTable.push(counter);
+//   }
+//   myTable.push(tempTable);
+// }
+// console.table(myTable);
 
+// 5.5
+// const gridArray = 64
+// let counter = 0
+// let rows = 4
 
+// 5.6
 
+const arr = [];
+let value = 1
 
+for (let i = 0; i < 10; i++) {
+  arr[i] = value;
+  value++
+}
+console.log(arr);
 
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (const val of arr) {
+  console.log(arr[val-1]);
+}
---
+++ JSB2P/2-my-sols/ch6.js
@@ -0,0 +1,50 @@
+// 6.1
+
+// const adder = (a, b) => a + b;
+// const x = 2;
+// const y = 3;
+// console.log(adder(x, y));
+// console.log(adder(4, 9));
+
+// 6.2
+// const arr = ['blue', 'cold', 'runny'];
+// const userName = prompt('What\'s your name?')
+// const randVal = Math.floor(Math.random() * arr.length)
+// console.log(userName)
+// console.log(arr[randVal]);
+
+// 6.3
+
+// const calculator = (a, b, operation) => {
+//   if (operation === 'add') {
+//     return a + b;
+//   } else if (operation === 'subtract') {
+//     return a - b;
+//   }
+// };
+
+// console.log(calculator(3, 4, 'subtract'))
+
+
+// 6.4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
