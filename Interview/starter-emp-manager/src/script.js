5d6c86c5961c4af807cfa082725efdb67355ffe2
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2023-01-15 21:21:14 +0000

    employee manager starter

---
+++ Interview/starter-emp-manager/src/script.js
@@ -0,0 +1,69 @@
+// IIFE function for security
+
+// fetch data in json format
+  
+  // by default first employee selected
+
+  // select DOM elements where to render
+
+  // Add Employee - START
+  // Add Employee - grab 3 selectors
+  
+  // createEmployee addEventListener to display modal
+
+  // close modal by clicking on grey area
+
+  // Set Employee age to be entered minimum 18 years
+
+  // submit employee form
+
+  // formData is an object with various methods
+
+  // values is an array of 8 arrays with the entered data
+
+  // emp data is an object populated with kay value pairs
+
+  // add id+1 to empData
+
+  // get age of employee
+
+  // add image to empData
+
+  // push empData to employees
+
+  // render employees
+
+  // reset form
+
+  // close modal
+
+  // Add Employee - END
+
+
+  // Select Employee Logic - START
+
+  // Select Employee Logic - END
+
+    // Employee Delete Logic - START
+    // return the employees not clicked on
+
+    // remove employee information section
+
+    // Employee Delete Logic - END
+
+
+  // Render All Employees Logic - START
+
+  // make sure it's empty first
+
+  // Render All Employees Logic - END
+
+  // Render Single Employee Logic - START
+
+  // Employee Delete Logic - START
+
+  // Employee Delete Logic - END
+
+
+  // Render Single Employee Logic - END
+
