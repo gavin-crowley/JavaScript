dbd0d392bedcb0798285b39b78c93f6d74c234b3
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2023-01-25 12:16:32 +0000

    Jan 25

--- JSB2P/sandbox.js
+++ JSB2P/sandbox.js
@@ -1,4 +1,5 @@
+let text = "I love JavaScript"
+console.log(text.match(/javascript/i));
 
-const words = ['blue', 'tall', 'sunny']
-const userPrompt = prompt('Enter your name:')
-
+let text2 = "In the end or at the beginning?";
+console.log(text2.match(/\bin/gi));
